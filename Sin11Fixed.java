ecc3ea4714675b8b0b2288fa7bb3ee76bb3cc48e
Jacob <jacobspaide@gmail.com>
2024-04-25 18:15:14 -0500

    added sin11

---
+++ Sin11Fixed.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Sin11Fixed {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        //ensure no negative number can be entered
+        if (number < 0) 
+        {
+            System.out.println("Error: Cannot calculate the square root of a negative number.");
+        } 
+        else 
+        {
+            double result = calculateSquareRoot(number);
+            System.out.println("Square root of " + number + " is: " + result);
+        }
+
+        scan.close();
+    }
+
+    public static double calculateSquareRoot(int number) {
+        //impossible for negative number to be entered into Math.sqrt() because 
+        //it is checked before this function call
+        return Math.sqrt(number);
+    }
+}
---
+++ Sin11Flawed.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Sin11Flawed {
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scan.nextInt();
+
+        double result = calculateSquareRoot(number);
+        System.out.println("Square root of " + number + " is: " + result);
+
+        scan.close();
+    }
+
+    public static double calculateSquareRoot(int number) 
+    {
+        //if the user enters a negative number, Math.sqrt() will return not a number
+        //because this program does not handle this error case explicitly.
+        return Math.sqrt(number);
+    }
+}
