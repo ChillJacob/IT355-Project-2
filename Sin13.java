f449726cb8455ff8a4166723c79b02e8a7df91ec
Jacob <jacobspaide@gmail.com>
2024-04-26 03:06:16 -0500

    Sin13 added

--- Sin13.java
+++ Sin13.java
@@ -2,7 +2,59 @@
 //Sin 13
 //Race Conditions
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Sin13
 {
+    private static int counter = 0;
+    //mutual exclusion lock
+    private static final Lock mutex = new ReentrantLock();
 
+    public static void main(String[] args) 
+    {
+        //creating two threads
+        Thread thread1 = new Thread(new IncrementTask());
+        Thread thread2 = new Thread(new IncrementTask());
+
+        thread1.start();
+        thread2.start();
+
+        try 
+        {
+            thread1.join();
+            thread2.join();
+        } 
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final counter value: " + counter);
+    }
+
+    private static class IncrementTask implements Runnable 
+    {
+        public void run() 
+        {
+            //each thread count to 1000
+            for (int i = 0; i < 1000; i++) 
+            {
+                incrementCounter();
+            }
+        }
+
+        private void incrementCounter() 
+        {
+            mutex.lock();
+            try 
+            {
+                counter++;
+            } 
+            finally 
+            {
+                mutex.unlock();
+            }
+        }
+    }
 }
